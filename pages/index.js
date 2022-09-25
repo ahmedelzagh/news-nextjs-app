256d838c0a566a92b470043397637203e807bf39
Ahmed Elzagh <ahmedelzagh50@gmail.com>
2022-09-25 19:17:56 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>News App</title>
+        <meta name="description" content="Read the Latest News." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
